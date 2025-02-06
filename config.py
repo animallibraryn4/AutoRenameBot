e51c5bec88ab905ce7633c22ca05617b8de75330
animallibraryn4 <nikhil7858978052@gmail.com>
2025-02-06 20:05:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7906213903:AAHCrLWgHppbCIdRm7kjnmovnBLIfCtPRVM") 
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME","codeflix")     
+    DB_NAME = os.environ.get("DB_NAME","Nt1")     
     DB_URL  = os.environ.get("DB_URL","mongodb+srv://Nt1:<db_password>@nt1.thwrt.mongodb.net/?retryWrites=true&w=majority&appName=NT1")
     PORT = os.environ.get("PORT", "25")
  
